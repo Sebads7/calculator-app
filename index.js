7a7dc0829c47eea1da2f6f3399dd86566c1f7d3c
Sebas <disalvatore.sebastian@gmail.com>
2024-10-29 14:46:43 -0300

    adding calculator functionalities

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const themeToggle = document.querySelector(".button-container");
 const themeSections = document.querySelectorAll(".theme-toggle");
-
 let theme = 1;
 
 fetch("constants/buttons.json")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
-
     const calculator = document.getElementById("calculator-buttons");
+    const display = document.getElementById("display");
 
     data.calculator.forEach((buttonData) => {
       const button = document.createElement("button");
@@ -20,12 +18,43 @@ fetch("constants/buttons.json")
       button.setAttribute("data-theme", theme);
 
       calculator.appendChild(button);
+
+      if (!isNaN(buttonData.id)) {
+        button.addEventListener("click", () => {
+          display.textContent += buttonData.button;
+        });
+      } else if (buttonData.id === "DEL") {
+        button.addEventListener("click", () => {
+          display.textContent = display.textContent.slice(0, -1);
+        });
+      } else if (buttonData.id === "RES") {
+        button.addEventListener("click", () => {
+          display.textContent = "";
+        });
+      } else if (buttonData.id === "=") {
+        button.addEventListener("click", () => {
+          try {
+            display.textContent = eval(display.textContent.replace("x", "*"));
+          } catch (error) {
+            display.textContent = "Error";
+          }
+        });
+      } else {
+        button.addEventListener("click", () => {
+          display.textContent +=
+            buttonData.button === "+"
+              ? "*"
+              : buttonData.button === " -"
+              ? "/"
+              : buttonData.button;
+        });
+      }
     });
   })
   .catch((error) => console.log("error", error));
 
 themeToggle.addEventListener("click", () => {
-  console.log("clicked");
+  // console.log("clicked");
   theme = theme < 3 ? theme + 1 : 1;
 
   themeSections.forEach((section) => {
@@ -38,5 +67,5 @@ themeToggle.addEventListener("click", () => {
     button.setAttribute("data-theme", theme);
   });
 
-  console.log(theme);
+  // console.log(theme);
 });
